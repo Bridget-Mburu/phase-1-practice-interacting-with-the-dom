98891409b78573d80db5f803418cdfa7c7086c3a
bridget <bridget.mburu.w@gmail.com>
2025-01-15 09:54:44 +0300

    update

---
+++ js/challenge.js
@@ -0,0 +1,39 @@
+let playing = !0
+function timer(setInterval) {
+    
+let counterTimer = document.getElementById('counter')
+ let minus = document.getElementById('minus')
+ let plus = document.querySelector('#plus')
+ let heart = document.querySelector('#heart')
+ let pause = document.getElementById('pause')
+ let commentForm = document.getElementsByTagName('form')
+
+ let interval = parseInt(counterTimer.innerText)
+
+
+
+ plus.addEventListener('click', (e) => adding (e))
+ const adding = (e) => {
+
+
+ counterTimer.innerText = interval + 1 
+
+ }
+
+ minus.addEventListener('click', (event) => subtracting (event))
+ const subtracting = (event) =>{
+counterTimer.innerText = interval - 1 
+
+ }
+
+heart.addEventListener('click', (event) => like (event))
+const like = (event) => {
+let like = document.querySelector('.likes')
+starts = 0
+
+if([].concat(_toConsumableArray))
+
+
+}
+
+}
